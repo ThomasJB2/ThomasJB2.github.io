39b99889f975f113ad788f1e659f01664f7ff480
Thomas <126576148+ThomasJB2@users.noreply.github.com>
2024-11-19 19:27:35 +0000

    new Update js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ function normalImg(x) {
   x.style.height = "20rem";
   x.style.width = "30rem";
 }
+
+var openPage = new Audio("/sounds/delta_flyer_door.mp3");
+
+var socialPress = new Audio("/sounds/helm_engage_clean.mp3");
+
+const btnPressSound = document.getElementById("btnPressSound");
+const btnPress = document.querySelectorAll("btnPress");
+btnPress.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    btnPressSound.play();
+  });
+});
