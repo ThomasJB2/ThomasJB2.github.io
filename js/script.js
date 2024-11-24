00fd83fd950ec94a15a5ced320084292a8e27d6e
Thomas <126576148+ThomasJB2@users.noreply.github.com>
2024-11-24 10:30:53 +0000

    Update all
    New slideshow on home page, update to all files including new CSS files

--- js/script.js
+++ js/script.js
@@ -12,11 +12,42 @@ var openPage = new Audio("/sounds/delta_flyer_door.mp3");
 
 var socialPress = new Audio("/sounds/helm_engage_clean.mp3");
 
-const btnPressSound = document.getElementById("btnPressSound");
-const btnPress = document.querySelectorAll("btnPress");
-btnPress.forEach((button) => {
-  button.addEventListener("click", function (event) {
-    event.preventDefault();
-    btnPressSound.play();
-  });
-});
+var btnPress = new Audio("/sounds/communications_start_transmission.mp3");
+
+// const btnPressSound = document.getElementById("btnPressSound");
+// const btnPress = document.querySelectorAll("btnPress");
+// btnPress.forEach((button) => {
+//   button.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     btnPressSound.play();
+//   });
+// });
+
+const slides = document.querySelectorAll(".ss-slides");
+let currentSlide = 0;
+
+// Function to show the slide
+function showSlide(index) {
+  // Ensure the index wraps around correctly
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+
+  // Hide all slides
+  slides.forEach((slide) => (slide.style.display = "none"));
+
+  // Show the current slide
+  slides[currentSlide].style.display = "block";
+}
+
+// Function to change the slide
+function changeSlide(step) {
+  showSlide(currentSlide + step);
+}
+
+// Initial setup to show the first slide
+showSlide(currentSlide);
